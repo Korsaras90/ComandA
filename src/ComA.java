72c8c8d8a29e24a3aa9314a393099b57506943c3
Korsaras90 <sergbirukof@gmail.com>
2020-11-07 16:37:57 +0300

    Initial commit

---
+++ src/ComA.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ComA {
+    public static void main(String[] args) {
+
+        int solution = 0;
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(); // Читаем с клавиатуры размер массива
+
+        int[] mas = new int[n]; //Создаём массив
+        in.nextLine();
+        for (int i = 0; i < n; i++) {
+            mas[i] = in.nextInt();   // Заполняем массив
+        }
+        for (int i = 0; i < n; i++) {
+            int r = 0;
+            while (mas[i] >= 10) {
+                int t = mas[i] % 10;
+                mas[i] = mas[i] / 10;
+                if (t == 1) {
+                    r++;
+                }
+                if (mas[i] == 1) {
+                    r++;
+                }
+                if (r >= 2) {
+                    solution++;
+                }
+            }
+        }
+        System.out.println("solution = " + solution);
+    }
+}
+
+
+
